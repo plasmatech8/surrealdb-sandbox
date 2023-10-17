634ccd4a86d7b32ed76ee26540183b7d4356b68c
Mark Connelly <com0004@gmail.com>
2023-10-17 16:41:35 +1100

    Remove menu dropdown

--- src/routes/HeaderContents.svelte
+++ src/routes/HeaderContents.svelte
@@ -19,42 +19,12 @@
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<LightSwitch />
-		<button class="btn btn-icon-sm hover:variant-soft-surface" use:popup={menuPopupSettings}>
-			<i class="fa-solid fa-ellipsis-vertical h-5" />
-		</button>
+		<a
+			class="btn btn-icon-sm hover:variant-soft-surface"
+			href="https://github.com/plasmatech8/surrealdb-sandbox"
+			target="_blank"
+		>
+			<i class="fa-brands fa-github h-5" />
+		</a>
 	</svelte:fragment>
 </AppBar>
-
-<nav class="list-nav card p-1 w-60 shadow-xl" data-popup={menuPopupSettings.target}>
-	<ul>
-		<li>
-			<button class="w-full justify-start">
-				<span class="badge bg-primary-500 aspect-square">
-					<i class="fas fa-floppy-disk text-white" />
-				</span>
-				<span class="">Save</span>
-			</button>
-		</li>
-
-		<li>
-			<button class="w-full justify-start">
-				<span class="badge bg-primary-500 aspect-square">
-					<i class="fas fa-file-arrow-down text-white" />
-				</span>
-				<span class="">Load</span>
-			</button>
-		</li>
-		<li>
-			<a
-				href="https://github.com/plasmatech8/skeleton-material-theme"
-				target="_blank"
-				class="focus:outline-none"
-			>
-				<span class="badge bg-primary-500 aspect-square">
-					<i class="fa-brands fa-github text-white" />
-				</span>
-				<span class="flex-auto">GitHub Repository</span>
-			</a>
-		</li>
-	</ul>
-</nav>
