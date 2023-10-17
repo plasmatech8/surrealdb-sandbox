8de8055f17e544ac4927a737d3c8a2d92fa53cf1
Mark Connelly <com0004@gmail.com>
2023-10-17 15:40:17 +1100

    Format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		forms,
@@ -16,10 +19,10 @@ export default {
 				preset: [
 					{
 						name: 'skeleton',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+						enhancements: true
+					}
+				]
+			}
+		})
+	]
 } satisfies Config;
