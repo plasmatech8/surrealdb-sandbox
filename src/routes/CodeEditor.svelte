96a3ae8c2d3a80d2d82359baf11398c1211d5d04
Mark Connelly <com0004@gmail.com>
2023-10-18 03:03:48 +1100

    Add tab feature

--- src/routes/CodeEditor.svelte
+++ src/routes/CodeEditor.svelte
@@ -5,6 +5,7 @@
 
 	let clientHeight: number;
 	let clientWidth: number;
+	let editorEl: HTMLDivElement;
 
 	const dispatch = createEventDispatcher();
 
@@ -17,6 +18,30 @@
 			e.preventDefault();
 			dispatch('save');
 		}
+		if (e.shiftKey && e.key == 'Tab') {
+			e.preventDefault();
+			changeTabSpacesForSelectedLines(-4);
+		} else if (e.key == 'Tab') {
+			e.preventDefault();
+			changeTabSpacesForSelectedLines(4);
+		}
+	}
+
+	function changeTabSpacesForSelectedLines(count: number) {
+		const selection = window.getSelection();
+		if (selection == null) return;
+		if (selection.rangeCount > 0) {
+			const range = selection.getRangeAt(0);
+			editorEl.childNodes.forEach((node) => {
+				if (range.intersectsNode(node)) {
+					if (count < 0) {
+						node.textContent = node.textContent?.replace(/^\s{0,4}/, '') as string;
+					} else {
+						node.textContent = '    ' + node.textContent;
+					}
+				}
+			});
+		}
 	}
 
 	const notypecheck = (x: any) => x;
@@ -24,8 +49,8 @@
 
 <div class="h-full relative overflow-clip" bind:clientHeight bind:clientWidth>
 	<div style="width: {clientWidth}px; height: {clientHeight}px;" class="absolute overflow-auto">
-		<!-- svelte-ignore -->
 		<div
+			bind:this={editorEl}
 			style="white-space: pre;"
 			role="textbox"
 			tabindex="0"
