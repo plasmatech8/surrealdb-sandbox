a1e75cb6e71762e8aa908be5735cc061e14ae791
Mark Connelly <com0004@gmail.com>
2023-10-18 12:34:47 +1100

    Fix browser error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import { ProgressRadial, getModalStore, getToastStore } from '@skeletonlabs/skeleton';
 	import { setLocalSave } from '$lib/localDatabaseSave';
 	import LoadDatabaseModal from '$lib/LoadDatabaseModal.svelte';
+	import { browser } from '$app/environment';
 
 	let modalStore = getModalStore();
 	let toastStore = getToastStore();
@@ -164,21 +165,23 @@
 	}
 
 	function getOS() {
-		let userAgent = window.navigator.userAgent.toLowerCase(),
-			macosPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i,
-			windowsPlatforms = /(win32|win64|windows|wince)/i,
-			iosPlatforms = /(iphone|ipad|ipod)/i,
-			os = null;
-		if (macosPlatforms.test(userAgent)) {
-			os = 'macos';
-		} else if (iosPlatforms.test(userAgent)) {
-			os = 'ios';
-		} else if (windowsPlatforms.test(userAgent)) {
-			os = 'windows';
-		} else if (/android/.test(userAgent)) {
-			os = 'android';
-		} else if (!os && /linux/.test(userAgent)) {
-			os = 'linux';
+		let os = null;
+		if (browser) {
+			let userAgent = window.navigator.userAgent.toLowerCase(),
+				macosPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i,
+				windowsPlatforms = /(win32|win64|windows|wince)/i,
+				iosPlatforms = /(iphone|ipad|ipod)/i;
+			if (macosPlatforms.test(userAgent)) {
+				os = 'macos';
+			} else if (iosPlatforms.test(userAgent)) {
+				os = 'ios';
+			} else if (windowsPlatforms.test(userAgent)) {
+				os = 'windows';
+			} else if (/android/.test(userAgent)) {
+				os = 'android';
+			} else if (!os && /linux/.test(userAgent)) {
+				os = 'linux';
+			}
 		}
 		return os;
 	}
